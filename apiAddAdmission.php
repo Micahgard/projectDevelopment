036a9ae5ee948e3d72af583aa8851b013501c013
unknown <wjddudrbs10045401@gmail.com>
2020-05-25 15:36:36 +1200

    Aaron

---
+++ apiAddAdmission.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Author: Aaron
+ * Date: 25/05/2020
+ * Version: 1.0
+ * Purpose: api for adding admission
+ */
+
+if (isset($_POST["description"])){      /*check the name of admission if it already existed in database*/
+    include_once "../class/Admission.php";
+    $admission = new Admission(null, $_POST["description"], $_POST["admissiondate"], $_POST["status"]);
+    $admission =save();
+    $msg = "new admission added";
+}else{
+    $msg = "admission already existed";
+}
+$msg = json_encode($msg);
+echo $msg;
---
+++ apiUpdateAdmission.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Author: Aaron
+ * Date: 25/05/2020
+ * Version: 1.0
+ * Purpose: api for updating admission
+ */
+
+if (isset($_POST["description"])){      /*check the name of admission if it already existed in database*/
+    include_once "../class/Admission.php";
+    $admission = new Admission(null, $_POST["description"], $_POST["admissiondate"], $_POST["status"]);
+    $admission =save();
+    $msg = "new admission updated";
+}else{
+    $msg = "admission already existed";
+}
+$msg = json_encode($msg);
+echo $msg;
