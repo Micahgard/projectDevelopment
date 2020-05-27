34769b4d54673fb6c6ad8334da8ac982b760ef28
Mojeeb <noutam01@myunitec.ac.nz>
2020-05-27 17:52:22 +1200

    aaron

---
+++ api/apiDeletePayment.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Author: Aaron
+ * Date: 27/05/2020
+ * Version: 1.0
+ * Purpose: api for deleting payment
+ */
+
+include_once "../class/Payment.php";
+
+if (isset($_GET['id'])) {
+    $payment = new Payment($_GET["id"], "", "");
+    $payment->delete();
+    $msg = "payment deleted";
+}else{
+    $msg = "payment not deleted";
+}
+$msg = json_encode($msg);
+echo $msg;
