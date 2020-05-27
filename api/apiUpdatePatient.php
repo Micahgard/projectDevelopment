64579d38117bd4c57194371a82d23c274482cd96
Micahgard <64760527+Micahgard@users.noreply.github.com>
2020-05-27 16:50:33 +1200

    New

---
+++ api/apiUpdatePatient.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Author: Micah
+ * Date: 27/05/2020
+ * Version: 1.0
+ * Purpose: api for updating patient
+ */
+
+include_once "../class/Patient.php";
+
+if (isset($_POST['id'])) {
+    $id = $_POST['id'];
+    $lastname = $_POST['last name'];
+    $firstname = $_POST['first name'];
+    $street = $_POST['street address'];
+    $suburb = $_POST['suburb'];
+    $city = $_POST['city'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $insurcode = $_POST['insurance code'];
+    $patient = new Ward($id, $lastname, $firstname, $street, $suburb, $city, $email, $phone, $insurcode);
+    $patient->update();
+    $msg = "patient updated";
+}else{
+    $msg = "patient not updated";
+}
+$msg = json_encode($msg);
+echo $msg;
