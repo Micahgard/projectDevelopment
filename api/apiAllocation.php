d4794219618e3ddc3feb807dd37dd91ad2ddac30
Micahgard <64760527+Micahgard@users.noreply.github.com>
2020-05-27 16:14:18 +1200

    apiAllocation

---
+++ api/apiAllocation.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Author: Micah
+ * Date: 27/05/2020
+ * Version: 1.0
+ * Purpose: api for Allocating a Doctor
+ */
+
+include_once "../Allocation.php";
+
+if (isset($_POST["name"])) {
+    $allocation = new Allocation(null, $_POST["fee"], $_POST["role"]);
+    $allocation->save();
+    $msg = "Allocation added";
+}else{
+    $msg = "Allocation not added";
+}
+$msg = json_encode($msg);
+echo $msg;
