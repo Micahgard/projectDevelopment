c09edf00a5c6ba555c979aa426de40dcef7a58c7
Mojeeb <noutam01@myunitec.ac.nz>
2020-05-27 17:03:26 +1200

    wakanda

--- api/apiAddAllocation.php
+++ api/apiAddAllocation.php
@@ -10,7 +10,7 @@ include_once "../class/Doctor.php";
 
 if (isset($_POST["name"])) {
     $doctor = new Doctor(null, $_POST["lastname"], $_POST["firstname"], $_POST["street"], $_POST["suburb"], $_POST["city"], $_POST["phone"], $_POST["speciality"], $_POST["salary"]);
-    $doctor->save();
+    $ward->save();
     $msg = "doctor added";
 }else{
     $msg = "doctor not added";
