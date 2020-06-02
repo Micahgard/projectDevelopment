d8f566971f1a3a96a13d7c499ca59f6ada27e8aa
BilmondNight <61954220+BilmondNight@users.noreply.github.com>
2020-06-02 17:26:57 +1200

    update medication

---
+++ api/apiUpdateMedication.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Author: Joel
+ * Date: 02/06/2020
+ * Version: 1.0
+ * Purpose: api for updating medication
+ */
+
+include_once "../class/Medication.php";
+
+if (isset($_POST['id'])){
+    $id = $_POST['id'];
+    $name = $_POST['name'];
+    $cost = $_POST['cost'];
+    $medication = new Medication($id, $name, $cost);
+    $medication->update();
+    $msg = "medication updated";
+}else{
+    $msg = "medication not updated";
+}
+$msg = json_encode($msg);
+echo $msg;
