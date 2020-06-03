989fc20447ca25148eb54709db4f257a1d209a75
Micahgard <64760527+Micahgard@users.noreply.github.com>
2020-06-03 17:16:39 +1200

    foreign key added

--- class/Allocation.php
+++ class/Allocation.php
@@ -1,8 +1,9 @@
 <?php
 /**
  * Author: Joel
+ * Updated: Micah
  * Date: 27/05/2020
- * Version: 1.1
+ * Version: 1.2
  * Purpose: class for allocation
  */
 include_once "DB.php";
@@ -11,10 +12,11 @@ class Allocation
     public $id;
     public $fee;
     public $role;
+    public $doctorid;
     public $dbconn;
 
 
-    public function __construct($id, $fee, $role)
+    public function __construct($id, $fee, $role, $doctorid)
     {
         $this->id = $id;
         $this->fee = $fee;
@@ -25,7 +27,7 @@ class Allocation
         //if I don't have this object in my database, I will register him first
         $this->dbconn = (new DB())->conn ;
         if (is_null($this->id)){
-            $query = "insert into Allocation values (null, $this->fee, '$this->role)'";
+            $query = "insert into Allocation values (null, '$this->fee', '$this->role', '$this->doctorid')";
             mysqli_query($this->dbconn, $query);
         }
         $this->dbconn->close();
@@ -34,7 +36,7 @@ class Allocation
     public function update(){
         $this->dbconn = (new DB())->conn ;
         if (!is_null($this->id)){
-            $query = "UPDATE Allocation SET fee=$this->fee, role='$this->role' WHERE AllocationID=$this->id";
+            $query = "UPDATE Allocation SET fee='$this->fee', role='$this->role', doctorID='$this->doctorid' WHERE AllocationID=$this->id";
             mysqli_query($this->dbconn, $query);
         }
         $this->dbconn->close();
