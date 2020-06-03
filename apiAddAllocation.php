6faf809db09f6fd08605e2ef215ee7755107e709
Micahgard <64760527+Micahgard@users.noreply.github.com>
2020-06-03 17:22:55 +1200

    added foreign key

---
+++ apiAddAllocation.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Author: Mojeeb
+ * Date: 27/05/2020
+ * Version: 1.0
+ * Purpose: api for adding allocation
+ */
+
+include_once "../class/Allocation.php";
+
+if (isset($_POST["fee"])) {
+    $allocation = new Allocation(null, $_POST["fee"], $_POST["role"], $_POST["doctorID"]);
+    $allocation->save();
+    $msg = "allocation added";
+}else{
+    $msg = "allocation not added";
+}
+$msg = json_encode($msg);
+echo $msg;
