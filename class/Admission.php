31965ea5f23c79c86242a45f723a8df5d711e316
BilmondNight <61954220+BilmondNight@users.noreply.github.com>
2020-06-03 16:21:37 +1200

    Add files via upload

--- class/Admission.php
+++ class/Admission.php
@@ -12,21 +12,25 @@ class Admission
     public $description;
     public $admissiondate;
     public $status;
+    public $patientID;
+    public $wardID;
     public $dbconn;
 
-    public function __construct($id, $description, $admissiondate, $status)
+    public function __construct($id, $description, $admissiondate, $status, $patientID, $wardID)
     {
         $this->id = $id;
         $this->description = $description;
         $this->admissiondate = $admissiondate;
         $this->status = $status;
+        $this->patientID = $patientID;
+        $this->wardID = $wardID;
     }
 
     public function save(){
         //if I don't have this object in my database, I will register him first
         $this->dbconn = (new DB())->conn ;
         if (is_null($this->id)){
-            $query = "insert into Admission values (null, '$this->description', '$this->admissiondate', '$this->status')";
+            $query = "insert into Admission values (null, '$this->description', '$this->admissiondate', '$this->status', $this->patientID, $this->wardID)";
             mysqli_query($this->dbconn, $query);
         }
         $this->dbconn->close();
@@ -35,7 +39,7 @@ class Admission
     public function update(){
         $this->dbconn = (new DB())->conn ;
         if (!is_null($this->id)){
-            $query = "UPDATE Admission SET description='$this->description', admissiondate='$this->admissiondate', status='$this->status' WHERE AdmissionID=$this->id";
+            $query = "UPDATE Admission SET description='$this->description', admissiondate='$this->admissiondate', status='$this->status', patientID=$this->patientID, wardID=$this->wardID WHERE AdmissionID=$this->id";
             mysqli_query($this->dbconn, $query);
         }
         $this->dbconn->close();
