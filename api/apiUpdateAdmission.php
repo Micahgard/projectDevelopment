6587b49d469386e01a692a22f19cd40d08178ee1
Micahgard <64760527+Micahgard@users.noreply.github.com>
2020-06-03 12:48:58 +1200

    new

--- api/apiUpdateAdmission.php
+++ api/apiUpdateAdmission.php
@@ -10,9 +10,9 @@ include_once "../class/Admission.php";
 
 if (isset($_POST['id'])) {
     $id = $_POST['id'];
-    $description = $_POST['last name'];
-    $admissiondate = $_POST['first name'];
-    $status = $_POST['street address'];
+    $description = $_POST['description'];
+    $admissiondate = $_POST['admissiondate'];
+    $status = $_POST['status'];
     $admission = new Admission($id, $description, $admissiondate, $status);
     $admission->update();
     $msg = "admission updated";
