e4111864b461c4dea4cedcd1580b4f043c9828b6
Jason <51270177+JasonL33T@users.noreply.github.com>
2020-06-04 17:41:41 +1200

    Add files via upload

--- api/apiLogin.php
+++ api/apiLogin.php
@@ -9,14 +9,12 @@ if (!empty($username) && !empty($password)) {
   $login = new Administrator();
   $login->login($username, $password);
 
-  echo $role;
-
-  /*switch($login->role) {
+  switch($login->role) {
     case 'senior':
       echo 'senior';
       break;
     case 'assistant':
       echo 'assistant';
       break;
-  }*/
+  }
 }
