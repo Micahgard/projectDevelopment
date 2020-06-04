e780172a694666e2a16f821698a98e44d4814d54
Mojeeb <noutam01@myunitec.ac.nz>
2020-06-04 16:47:45 +1200

    'a'

---
+++ pages/login.php
@@ -0,0 +1,10 @@
+<?php
+include_once 'head.php';
+?>
+
+<form>
+    <label for="username">Username: </label></td>
+    <input type="text" id="username" name="username">
+    <label for="password">Password: </label></td>
+    <input type="text" id="password" name="password">
+</form>
