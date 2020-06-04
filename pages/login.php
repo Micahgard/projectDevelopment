5dc0ec12abea35760ffd1507205f79a9e52c64bf
Mojeeb <noutam01@myunitec.ac.nz>
2020-06-04 16:52:27 +1200

    'a'

--- pages/login.php
+++ pages/login.php
@@ -2,9 +2,10 @@
 include_once 'head.php';
 ?>
 
-<form>
+<form action="../api/login.php" method="post">
     <label for="username">Username: </label></td>
     <input type="text" id="username" name="username">
     <label for="password">Password: </label></td>
     <input type="text" id="password" name="password">
+    <input type="submit" name="submit" value="Submit">
 </form>
