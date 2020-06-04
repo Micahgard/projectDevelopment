06908214679082e641b1d376e306be334e9e8cdb
Mojeeb <noutam01@myunitec.ac.nz>
2020-06-04 16:59:11 +1200

    forever

--- pages/login.php
+++ pages/login.php
@@ -3,9 +3,20 @@ include_once 'head.php';
 ?>
 
 <form action="../api/login.php" method="post">
-    <label for="username">Username: </label></td>
-    <input type="text" id="username" name="username"><br>
-    <label for="password">Password: </label></td>
-    <input type="text" id="password" name="password"><br>
-    <input type="submit" name="submit" value="Submit">
+    <h2>Update Admission</h2>
+    <table class="table-sm table-borderless">
+        <tbody>
+        <tr>
+            <td><label for="username">Username: </label></td>
+            <td><input type="text" id="username" name="username"></td>
+        </tr>
+        <tr>
+            <td><label for="password">Password: </label></td>
+            <td><input type="text" id="password" name="password"></td>
+        </tr>
+        <tr>
+            <td><input type="submit" name="submit" value="Submit"></td>
+        </tr>
+        </tbody>
+    </table>
 </form>
