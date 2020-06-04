39988680e31b6a4d4caeca96869d1f79c86571dc
Jason <51270177+JasonL33T@users.noreply.github.com>
2020-06-04 17:36:07 +1200

    Add files via upload

--- api/apiLogin.php
+++ api/apiLogin.php
@@ -1,22 +1,20 @@
 <?php
 
+include_once "../class/Administrator.php";
+
 $username = $_POST['username'];
 $password = $_POST['password'];
 
-//$admin = new Administrator();
-//$admin->login();
-//
-//if (!empty($username) & !empty($password)) {
-//
-//}else {
-//
-//}
-if (isset($_POST["username"])) {
-    $admission = new Administrator(null, $_POST["description"], $_POST["admissiondate"], $_POST["status"], $_POST["patientID"], $_POST["wardID"]);
-    $admission->save();
-    $msg = "Admission added";
-}else{
-    $msg = "Admission not added";
+if (!empty($username) && !empty($password)) {
+  $login = new Administrator();
+  $login->login($username, $password);
+
+  switch($login->role) {
+    case 'senior':
+      echo 'senior';
+      break;
+    case 'assistant':
+      echo 'assistant';
+      break;
+  }
 }
-$msg = json_encode($msg);
-echo $msg;
