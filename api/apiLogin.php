1dda831ecac2dce84facdb89a3787877d5fda2da
Lei Song <33405093+songlei1979@users.noreply.github.com>
2020-06-08 14:05:01 +1200

    new login page

--- api/apiLogin.php
+++ api/apiLogin.php
@@ -6,27 +6,37 @@ $username = $_POST['username'];
 $password = $_POST['password'];
 
 if (!empty($username) && !empty($password)) {
-  $login = new Administrator();
-  $login->login($username, $password);
+    $login = new Administrator();
+    $login->login($username, $password);
 
-  switch($login->role) {
-    case 'senior':
-      echo 'senior';
-      break;
-    case 'assistant':
-      echo 'assistant';
-      break;
-    case 'facility':
-      echo 'facility';
-      break;
-    case 'pharmacy':
-      echo 'pharmacy';
-      break;
-    case 'research':
-      echo 'research';
-      break;
-    case 'clerk':
-      echo 'clerk';
-      break;
-  }
+    switch($login->role) {
+        case 'senior':
+            echo 'senior';
+            break;
+        case 'assistant':
+            echo 'assistant';
+            ?>
+    <div class="block">
+        <p><a href="page/addAdmission.php">Add Admission</a></p>
+        <p><a href="page/admissionReport.php">Admission Report</a></p>
+        <p><a href="page/updateAdmission.php">Update Admission</a></p>
+        <p><a href="page/closeAdmission.php">Close Admission</a></p>
+        <p><a href="page/deleteAdmission.php">Delete Admission</a></p>
+    </div>
+<?php
+            break;
+        case 'facility':
+            echo 'facility';
+            break;
+        case 'pharmacy':
+            echo 'pharmacy';
+            break;
+        case 'research':
+            echo 'research';
+            break;
+        case 'clerk':
+            echo 'clerk';
+            break;
+    }
 }
+
