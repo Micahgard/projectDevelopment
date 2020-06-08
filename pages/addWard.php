5d6eefde00e7964f9957e1c1129682156cc90451
BilmondNight <61954220+BilmondNight@users.noreply.github.com>
2020-06-08 15:19:55 +1200

    add ward form

--- pages/addWard.php
+++ pages/addWard.php
@@ -18,17 +18,15 @@
 
         <tr>
             <td><label>capacity:* </label></td>
-            <td><select name="capacity" size="5">
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4">4</option>
-                    <option value="5">5</option>
-                    <option value="6">6</option>
-                    <option value="7">7</option>
-                    <option value="8">8</option>
-                    <option value="9">9</option>
-                    <option value="10">10</option>
+            <td><select name="capacity">
+                    <?php
+                    $i = 1;
+                    while ($i<=20){
+                        echo '<option value="$i">$i</option>';
+                        $i = $i + 1;
+                    }
+
+                    ?>
                 </select>
             </td>
         </tr>
