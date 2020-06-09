945244ce738fdfbd1e453a1d31eebf6d9288a8c2
BilmondNight <61954220+BilmondNight@users.noreply.github.com>
2020-06-09 16:10:48 +1200

    Add files via upload

--- pages/addDoctor.php
+++ pages/addDoctor.php
@@ -1,64 +1,65 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-?>
+<head>
+    <title>Add Doctor</title>
+    <?php include_once 'head.php'; ?>
+</head>
 
-    <form action="../api/apiAddDoctor.php" method="post">
-        <h2>Add Doctor</h2>
-        <table class="table-sm table-borderless">
-            <tbody>
-            <tr>
-                <td><label>Last Name:* </label></td>
-                <td><input type="text" id="lastname" name="lastname" size="25" required></td>
-            </tr>
+<body>
 
-            <tr>
-                <td><label>First Name:* </label></td>
-                <td><input type="text" id="firstname" name="firstname" size="25" required></td>
-            </tr>
+<form action="../api/apiAddDoctor.php" method="post">
 
-            <tr>
-                <td><label>Street Address:* </label></td>
-                <td><input type="text" id="street" name="street" size="50" required></td>
-            </tr>
+    <h2>Add Doctor</h2>
+    <table>
+        <tr>
+            <td><label>Last Name:* </label></td>
+            <td><input type="text" id="lastname" name="lastname" size="25" required></td>
+        </tr>
 
-            <tr>
-                <td><label>Suburb:* </label></td>
-                <td><input type="text" id="suburb" name="suburb" size="20" required></td>
-            </tr>
+        <tr>
+            <td><label>First Name:* </label></td>
+            <td><input type="text" id="firstname" name="firstname" size="25" required></td>
+        </tr>
 
-            <tr>
-                <td><label>City:* </label></td>
-                <td><input type="text" id="city" name="city" size="20" required></td>
-            </tr>
+        <tr>
+            <td><label>Street Address:* </label></td>
+            <td><input type="text" id="street" name="street" size="50" required></td>
+        </tr>
 
-            <tr>
-                <td><label>Email Address:* </label></td>
-                <td><input type="email" id="email" name="email" size="30" required></td>
-            </tr>
+        <tr>
+            <td><label>Suburb:* </label></td>
+            <td><input type="text" id="suburb" name="suburb" size="20" required></td>
+        </tr>
 
-            <tr>
-                <td><label>Phone Number:* </label></td>
-                <td><input type="text" id="phone" name="phone" size="15" required></td>
-            </tr>
+        <tr>
+            <td><label>City:* </label></td>
+            <td><input type="text" id="city" name="city" size="20" required></td>
+        </tr>
 
-            <tr>
-                <td><label>Speciality* </label></td>
-                <td><input type="text" id="speciality" name="speciality" size="20" required></td>
-            </tr>
+        <tr>
+            <td><label>Phone Number:* </label></td>
+            <td><input type="text" id="phone" name="phone" size="15" required></td>
+        </tr>
 
-            <tr>
-                <td><label>Salary* </label></td>
-                <td><input type="text" id="salary" name="salary" size="20" required></td>
-            </tr>
+        <tr>
+            <td><label>Speciality:* </label></td>
+            <td><input type="text" id="speciality" name="speciality" size="15" required></td>
+        </tr>
 
+        <tr>
+            <td><label>Salary:* </label></td>
+            <td><input type="text" id="salary" name="salary" size="15" required></td>
+        </tr>
 
+        <tr><td><i style="color: red">* Required Fields</i></td></tr>
+        <tr>
+            <td><input class="btn" type="submit" value="Add Doctor"/></td>
+            <td><a href="../api/apiLogin.php"><input class="btn" type="button" value="Return"></a></td>
+        </tr>
+    </table>
+</form>
 
-            <tr><td><i style="color: red">* Required Fields</i></td></tr>
-            <tr>
-                <td><input type="submit" name="addDoctor" value="Add Doctor"/></td>
-                <td><input type="submit" name="return" value="Return"></td>
-            </tr>
-            </tbody>
-        </table>
-    </form>
-<?php
+<div class="foot">
+    <?php include_once 'foot.php'; ?>
+</div>
--- pages/addResearchtopic.php
+++ pages/addResearchtopic.php
@@ -1,36 +1,44 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-?>
+<head>
+    <title>Add Research Topic</title>
+    <?php include_once 'head.php'; ?>
+</head>
 
-    <form action="../api/apiAddResearchtopic.php" method="post">
-        <h2>Add Research Topic</h2>
-        <table class="table-sm table-borderless">
-            <tbody>
-            <tr>
-                <td><label>Description* </label></td>
-                <td><input type="text" id="description" name="description" size="50" required></td>
-            </tr>
+<body>
 
-            <tr>
-                <td><label>level:* </label></td>
-                <td><select name="level">
-                        <?php
-                        $i = 1;
-                        while ($i<=10){
-                            echo "<option value='".$i."'>".$i."</option>";
-                            $i = $i + 1;
-                        }
-                        ?>
-                    </select>
-                </td>
-            </tr>
+<form action="../api/apiAddResearchtopic.php" method="post">
 
-            <tr><td><i style="color: red">* Required Fields</i></td></tr>
-            <tr>
-                <td><input type="submit" name="researchtopic" value="Add research topic"/></td>
-                <td><input type="submit" name="return" value="Return"></td>
-            </tr>
-            </tbody>
-        </table>
-    </form>
-<?php
+    <h2>Add Research Topic</h2>
+    <table>
+        <tr>
+            <td><label>Description* </label></td>
+            <td><input type="text" id="description" name="description" size="50" required></td>
+        </tr>
+
+        <tr>
+            <td><label>level:* </label></td>
+            <td><select name="level">
+                    <?php
+                    $i = 1;
+                    while ($i<=10){
+                        echo "<option value='".$i."'>".$i."</option>";
+                        $i = $i + 1;
+                    }
+                    ?>
+                </select>
+            </td>
+        </tr>
+
+        <tr><td><i style="color: red">* Required Fields</i></td></tr>
+        <tr>
+            <td><input class="btn" type="submit" value="Add Topic"/></td>
+            <td><a href="../api/apiLogin.php"><input class="btn" type="button" value="Return"></a></td>
+        </tr>
+    </table>
+</form>
+
+<div class="foot">
+    <?php include_once 'foot.php'; ?>
+</div>
