caeededb47b1d9abec8767aa0401ec9c65b496bb
Mojeeb <noutam01@myunitec.ac.nz>
2020-06-10 16:01:21 +1200

    a

--- pages/head.php
+++ pages/head.php
@@ -6,16 +6,13 @@
     <link href="../css/global.css" rel="stylesheet">
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
-
     <!-- jQuery library -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-
     <!-- Popper JS -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-
     <!-- Latest compiled JavaScript -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
-    <link rel="icon" src="../image/icon.png">
+    <link rel="icon" href="../image/icon.png">
 </head>
 <body>
     <img class="headimg" src="../image/head.jpg" alt="image error">
