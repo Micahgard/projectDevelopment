f3cfe4d47be2507e503a7f9c87a15713ed34b630
Mojeeb <noutam01@myunitec.ac.nz>
2020-06-10 15:30:23 +1200

    footer

--- pages/foot.php
+++ pages/foot.php
@@ -1,4 +1,7 @@
-        </div>
-    </body>
+</div>
+<footer class="page-footer font-small blue">
+    <div class="footer-copyright text-center py-3">© 2020 Copyright:
+        <a href="https://mdbootstrap.com/"> Saint Albert Hospital</a>
+    </div>
+</footer>
 </html>
-&copy; Saint Albert Hospital 1964 - 2020
