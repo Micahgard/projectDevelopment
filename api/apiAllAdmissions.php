656cec3aabae519077bfc3b8e702a0327e45133a
Lei Song <33405093+songlei1979@users.noreply.github.com>
2020-06-10 17:21:22 +1200

    new show all admissions api

--- api/apiAllAdmissions.php
+++ api/apiAllAdmissions.php
@@ -2,7 +2,7 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-include_once "class/Administrator.php";
+include_once "../class/Administrator.php";
 $admin = new Administrator();
 $admissions = $admin->showAdmission();
 
