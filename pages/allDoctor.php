dfc38a422d4e29e3c2b9267c36f06aef8abe5763
Mojeeb <noutam01@myunitec.ac.nz>
2020-06-10 16:48:19 +1200

    a

--- pages/allDoctor.php
+++ pages/allDoctor.php
@@ -4,7 +4,7 @@
 <!--Nav Tabs-->
 <ul class="nav nav-tabs" role="tablist">
     <li class="nav-item">
-        <a class="nav-link active" data-toggle="tab" href="#home">Home</a>
+        <a class="nav-link active" data-toggle="tab" href="#addDoctor">Add Doctor</a>
     </li>
     <li class="nav-item">
         <a class="nav-link" data-toggle="tab" href="#menu1">Menu 1</a>
@@ -17,8 +17,56 @@
 <!-- Tab panes -->
 <div class="tab-content">
     <div id="home" class="container tab-pane active"><br>
-        <h3>HOME</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        <form action="../api/apiAddDoctor.php" method="post">
+            <h2>Add Doctor</h2>
+            <table>
+                <tr>
+                    <td><label>Last Name:* </label></td>
+                    <td><input type="text" id="lastname" name="lastname" size="25" required></td>
+                </tr>
+
+                <tr>
+                    <td><label>First Name:* </label></td>
+                    <td><input type="text" id="firstname" name="firstname" size="25" required></td>
+                </tr>
+
+                <tr>
+                    <td><label>Street Address:* </label></td>
+                    <td><input type="text" id="street" name="street" size="50" required></td>
+                </tr>
+
+                <tr>
+                    <td><label>Suburb:* </label></td>
+                    <td><input type="text" id="suburb" name="suburb" size="20" required></td>
+                </tr>
+
+                <tr>
+                    <td><label>City:* </label></td>
+                    <td><input type="text" id="city" name="city" size="20" required></td>
+                </tr>
+
+                <tr>
+                    <td><label>Phone Number:* </label></td>
+                    <td><input type="text" id="phone" name="phone" size="15" required></td>
+                </tr>
+
+                <tr>
+                    <td><label>Speciality:* </label></td>
+                    <td><input type="text" id="speciality" name="speciality" size="15" required></td>
+                </tr>
+
+                <tr>
+                    <td><label>Salary:* </label></td>
+                    <td><input type="text" id="salary" name="salary" size="15" required></td>
+                </tr>
+
+                <tr><td><i style="color: red">* Required Fields</i></td></tr>
+                <tr>
+                    <td><input class="btn" type="submit" value="Add Doctor"/></td>
+                    <td><a href="../api/apiLogin.php"><input class="btn" type="button" value="Return"></a></td>
+                </tr>
+            </table>
+        </form>
     </div>
     <div id="menu1" class="container tab-pane fade"><br>
         <h3>Menu 1</h3>
