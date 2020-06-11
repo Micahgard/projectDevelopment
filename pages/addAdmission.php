63d2ac31f837c2baf0465d6d9dc9cdeeadaa3f14
BilmondNight <61954220+BilmondNight@users.noreply.github.com>
2020-06-11 18:14:33 +1200

    Add files via upload

--- pages/addAdmission.php
+++ pages/addAdmission.php
@@ -4,17 +4,30 @@
         $(document).ready(function () {
             $.ajax({
                 type: 'GET',
-                url: "https://unitecproject.herokuapp.com/api/apiAllAdmissions.php",
+                url: "https://unitecproject.herokuapp.com/api/apiAllPatients.php",
                 dataType: "JSON",
                 success: function (data) {
-                    $("#patientInfo").append("<select name='patient' class='custom-select'>");
                     let i = 0;
                     while (i < data.length){
-                        $("#patientInfo").append("<option value='" + data[i].patientID + "'>" + data[i].patientID + "</option>");
-                        // $("#patientinfo").append("<p>patient info: " + data[i].patientID + "</p>");
+                       $("#getPatient").append("<option value='" + data[i].id + "'>" + data[i].id + " " + data[i].firstname + " " + data[i].lastname + "</option>");
                         i = i + 1;
                     }
-                    $("#PatientInfo").append("</select>");
+                },
+                error: function () {
+                    alert("Not connected");
+                }
+            });
+
+            $.ajax({
+                type: 'GET',
+                url: "https://unitecproject.herokuapp.com/api/apiAllWards.php",
+                dataType: "JSON",
+                success: function (data) {
+                    let i = 0;
+                    while (i < data.length){
+                        $("#getWard").append("<option value='" + data[i].id + "'>" + data[i].id + " " + data[i].name + "</option>");
+                        i = i + 1;
+                    }
                 },
                 error: function () {
                     alert("Not connected");
@@ -22,6 +35,7 @@
             });
         });
     </script>
+<body>
 <form action="../api/apiAddAdmission.php" method="post">
     <h2>Add Admission</h2>
     <table>
@@ -37,7 +51,12 @@
 
         <tr>
             <td><label>Patient:* </label></td>
-            <td><label id="patientInfo"></label></td>
+            <td><select id="getPatient" name="patientID"></select></td>
+        </tr>
+
+        <tr>
+            <td><label>Ward:* </label></td>
+            <td><select id="getWard" name="wardID"></select></td>
         </tr>
 
         <tr><td><i style="color: red">* Required Fields</i></td></tr>
