859f4124892c4d2ecbdc06e3e67344c8d43a0753
BilmondNight <61954220+BilmondNight@users.noreply.github.com>
2020-06-11 16:35:13 +1200

    Add files via upload

--- api/apiAllPatients.php
+++ api/apiAllPatients.php
@@ -1,5 +1,5 @@
 <?php
-include_once "../class/Patient.php";
+include_once "../class/Administrator.php";
 $admin = new Administrator();
 $patients = $admin->allPatiens();
 echo json_encode($patients);
