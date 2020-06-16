babb779f854120193195f78f5b30888bfc08b21d
Mojeeb <noutam01@myunitec.ac.nz>
2020-06-16 20:12:18 +1200

    a

--- pages/allAdmission.php
+++ pages/allAdmission.php
@@ -104,16 +104,60 @@
             $(document).ready(function () {
                 $.ajax({
                     type: 'GET',
+                    url: "../api/apiAllPatients.php",
+                    dataType: "JSON",
+                    success: function (data) {
+                        let i = 0;
+                        while (i < data.length){
+                            $("#getPatient").append("<option value='" + data[i].id + "'>" + data[i].id + " " + data[i].firstname + " " + data[i].lastname + "</option>");
+                            i = i + 1;
+                        }
+                    },
+                    error: function () {
+                        alert("Not connected");
+                    }
+                });
+                $.ajax({
+                    type: 'GET',
+                    url: "../api/apiAllWards.php",
+                    dataType: "JSON",
+                    success: function (data) {
+                        let i = 0;
+                        while (i < data.length){
+                            $("#getWard").append("<option value='" + data[i].id + "'>" + data[i].id + " " + data[i].name + "</option>");
+                            i = i + 1;
+                        }
+                    },
+                    error: function () {
+                        alert("Not connected");
+                    }
+                });
+                $.ajax({
+                    type: 'GET',
                     url: "../api/apiAllAdmissions.php",
                     dataType: "JSON",
                     success: function (data) {
-                        $("#patientInfo").append("<select name='patient' class='custom-select'>");
                         let i = 0;
                         while (i < data.length){
-                            $("#patientInfo").append("<option value='" + data[i].patientID + "'>" + data[i].patientID + "</option>");
+                            $("#getUpdateAdmissions").append("<option value='" + data[i].id + "'>" + data[i].id + " " + data[i].description + "</option>");
                             i = i + 1;
                         }
-                        $("#PatientInfo").append("</select>");
+                        $("#getUpdateAdmissions").change(function() {
+                            var i = 0;
+                            while (i < data.length) {
+                                if (data[i].id == $("#getUpdateAdmissions").val()) {
+                                    $("#updateAdmissionId").val(data[i].id);
+                                    $("#updateDescription").val(data[i].description);
+                                    $("#updateStreet").val(data[i].street);
+                                    $("#updateSuburb").val(data[i].suburb);
+                                    $("#updateCity").val(data[i].city);
+                                    $("#updatePhone").val(data[i].phone);
+                                    $("#updateSpeciality").val(data[i].speciality);
+                                    $("#updateSalary").val(data[i].salary);
+                                }
+                                i++;
+                            }
+                        });
                     },
                     error: function () {
                         alert("Not connected");
@@ -123,28 +167,55 @@
         </script>
         <form action="../api/apiUpdateAdmission.php" method="post">
             <h2>Update Admission</h2>
-            <table>
-                <tr>
-                    <td><label>Description:<b class="red">*</b> </label></td>
-                    <td><input type="text" id="updateDescription" name="description" size="30" required></td>
-                </tr>
-
-                <tr>
-                    <td><label>Admission Date:<b class="red">*</b> </label></td>
-                    <td><input type="date" id="updateAdmissionDate" name="admissiondate" required></td>
-                </tr>
-
-                <tr>
-                    <td><label>Patient:<b class="red">*</b> </label></td>
-                    <td><label id="updatePatientInfo"></label></td>
-                </tr>
-
-                <tr><td><i class="red">* Required Fields</i></td></tr>
-                <tr>
-                    <td><input class="btn btn-outline-primary" type="submit" value="Update Admission"/></td>
-                    <td><a href="../api/apiLogin.php"><input class="btn btn-outline-primary" type="button" value="Return"></a></td>
-                </tr>
-            </table>
+            <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                    <span class="input-group-text">Admissions:* </span>
+                </div>
+                <select class="form-control" id="getUpdateAdmissions" name="id" required>
+                    <option disabled selected hidden>Select a Doctor</option>
+                </select>
+            </div>
+            <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                    <span class="input-group-text">Admission Id: </span>
+                </div>
+                <input type="text" maxlength="30" class="form-control" id="updateAdmissionId" name="updateAdmissionId" title="Admission Id" readonly>
+                <div class="input-group-prepend">
+                    <span class="input-group-text">Description:* </span>
+                </div>
+                <input type="text" maxlength="30" class="form-control" id="updateDescription" name="updateDescription" placeholder="Description*" title="Description" required>
+            </div>
+            <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                    <span class="input-group-text">Admission Date:* </span>
+                </div>
+                <input type="date" maxlength="10" class="form-control" id="updateAdmissiondate" name="updateAdmissiondate" placeholder="Admission Date" title="Admission Date" required>
+                <div class="input-group-prepend">
+                    <span class="input-group-text">Status:* </span>
+                </div>
+                <select class="form-control" id="updateStatus" name="updateStatus" required>
+                    <option disabled selected hidden>Select a Status</option>
+                    <option value="current">Current</option>
+                    <option value="complete">Complete</option>
+                    <option value="billed">Billed</option>
+                    <option value="closed">Closed</option>
+                </select>
+            </div>
+            <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                    <span class="input-group-text">Patient:* </span>
+                </div>
+                <select class="form-control" id="getPatient" name="patientID" required></select>
+                <div class="input-group-prepend">
+                    <span class="input-group-text">Ward:* </span>
+                </div>
+                <select class="form-control" id="getWard" name="wardID" required></select>
+            </div>
+            <i class="grey">* Required Fields</i>
+            <div class="d-flex justify-content-around">
+                <input class="btn btn-outline-primary" type="submit" value="Add Admission"/>
+                <a href="../api/apiLogin.php"><input class="btn btn-outline-primary" type="button" value="Return"></a>
+            </div>
         </form>
     </div>
 
