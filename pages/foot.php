17594bd8f5c5c06ac8e0cb42cd0e5afae090d8c8
Mojeeb <noutam01@myunitec.ac.nz>
2020-06-16 18:45:55 +1200

    a

--- pages/foot.php
+++ pages/foot.php
@@ -1,6 +1,6 @@
 </div>
 <footer class="footer bg-primary">
-    <div class="text-center text-white">© 2020 Copyright | Saint Albert Hospital</div>
+    <div class="text-center text-white">© 2020 Copyright - Saint Albert Hospital</div>
 </footer>
 </body>
 </html>
