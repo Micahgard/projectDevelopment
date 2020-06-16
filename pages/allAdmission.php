964fe1359bc3b3dfe9004bcb6e36b7d4443a52c2
Mojeeb <noutam01@myunitec.ac.nz>
2020-06-16 19:56:12 +1200

    a

--- pages/allAdmission.php
+++ pages/allAdmission.php
@@ -56,33 +56,47 @@
         </script>
         <form action="../api/apiAddAdmission.php" method="post">
             <h2>Add Admission</h2>
-            <table>
-                <tr>
-                    <td><label>Description:<b class="red">*</b> </label></td>
-                    <td><input type="text" id="description" name="description" size="30" required></td>
-                </tr>
-
-                <tr>
-                    <td><label>Admission Date:<b class="red">*</b> </label></td>
-                    <td><input type="date" id="admissiondate" name="admissiondate" required></td>
-                </tr>
-
-                <tr>
-                    <td><label>Patient:<b class="red">*</b> </label></td>
-                    <td><select id="getPatient" name="patientID"><option></option></select></td>
-                </tr>
-
-                <tr>
-                    <td><label>Ward:<b class="red">*</b> </label></td>
-                    <td><select id="getWard" name="wardID"><option></option></select></td>
-                </tr>
-
-                <tr><td><i class="red">* Required Fields</i></td></tr>
-                <tr>
-                    <td><input class="btn btn-outline-primary" type="submit" value="Add Admission"/></td>
-                    <td><a href="../api/apiLogin.php"><input class="btn btn-outline-primary" type="button" value="Return"></a></td>
-                </tr>
-            </table>
+            <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                    <span class="input-group-text">Description:* </span>
+                </div>
+                <input type="text" maxlength="30" class="form-control" id="description" name="description" placeholder="Description*" title="Description" required>
+            </div>
+            <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                    <span class="input-group-text">Admission Date:* </span>
+                </div>
+                <input type="date" maxlength="10" class="form-control" id="admissiondate" name="admissiondate" placeholder="Admission Date" title="Admission Date" required>
+                <div class="input-group-prepend">
+                    <span class="input-group-text">Status:* </span>
+                </div>
+                <select class="form-control" id="status" name="status" required>
+                    <option disabled selected hidden>Select a Status</option>
+                    <option value="current">Current</option>
+                    <option value="complete">Complete</option>
+                    <option value="billed">Billed</option>
+                    <option value="closed">Closed</option>
+                </select>
+            </div>
+            <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                    <span class="input-group-text">Patient: </span>
+                </div>
+                <select id="getPatient" name="patientID" required>
+                    <option disabled selected hidden>Select a Patient</option>
+                </select>
+                <div class="input-group-prepend">
+                    <span class="input-group-text">Ward: </span>
+                </div>
+                <select id="getWard" name="wardID" required>
+                    <option disabled selected hidden>Select a Ward</option>
+                </select>
+            </div>
+            <i class="grey">* Required Fields</i>
+            <div class="d-flex justify-content-around">
+                <input class="btn btn-outline-primary" type="submit" value="Add Admission"/>
+                <a href="../api/apiLogin.php"><input class="btn btn-outline-primary" type="button" value="Return"></a>
+            </div>
         </form>
     </div>
 
