8ab66d72fad5b7e0ff2f13bd856dd9dcc7d01cdc
Mojeeb <noutam01@myunitec.ac.nz>
2020-06-22 23:05:04 +1200

    a

--- api/apiAddDoctor.php
+++ api/apiAddDoctor.php
@@ -11,8 +11,20 @@ include_once "../class/Doctor.php";
 if (isset($_POST["lastname"])) {
     $doctor = new Doctor(null, $_POST["lastname"], $_POST["firstname"], $_POST["street"], $_POST["suburb"], $_POST["city"], $_POST["phone"], $_POST["speciality"], $_POST["salary"]);
     $doctor->save();
+    function goBack() {
+        window.history.back();
+    }
+    ?>
+    <div class="alert alert-primary alert-dismissible fade show">
+        <button type="button" class="close" data-dismiss="alert">&times;</button>
+        <strong>Primary!</strong> Indicates an important action.
+    </div>
+    <?php
     $msg = "doctor added";
 }else{
+    ?>
+    <a class="btn btn-danger" onclick="toastr.error('Hi! I am error message.');">Error message</a>
+    <?php
     $msg = "doctor not added";
 }
 $msg = json_encode($msg);
