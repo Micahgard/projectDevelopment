d0df7c2884445922a093c4ee31ea17594c27c015
Mojeeb <noutam01@myunitec.ac.nz>
2020-06-22 23:59:28 +1200

    a

--- pages/allDoctor.php
+++ pages/allDoctor.php
@@ -20,7 +20,7 @@
 <!-- Tab panes -->
 <div class="tab-content">
     <div id="addDoctor" class="container tab-pane active"><br>
-        <script>
+        <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js">
             $(document).ready(function(){
                 if (addSuccess == true) {
                     $('.alert').toggleClass('display-none');
