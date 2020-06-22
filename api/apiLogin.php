51982d46951b0aeae3c6d60361a9523cf6ee0102
Mojeeb <noutam01@myunitec.ac.nz>
2020-06-22 21:22:00 +1200

    a

--- api/apiLogin.php
+++
@@ -1,9 +0,0 @@
-<?php
-include_once "../class/Administrator.php";
-
-$username = $_POST['username'];
-$password = $_POST['password'];
-
-include_once '../pages/head.php';
-
-include_once '../pages/foot.php';
---
+++ pages/index.php
@@ -0,0 +1,9 @@
+<?php
+include_once "../class/Administrator.php";
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+include_once '../pages/head.php';
+
+include_once '../pages/foot.php';
