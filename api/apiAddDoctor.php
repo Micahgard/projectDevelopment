1ea6bed7ee485b0c45df77ea1f710668e723fffc
Mojeeb <noutam01@myunitec.ac.nz>
2020-06-22 23:43:51 +1200

    a

--- api/apiAddDoctor.php
+++ api/apiAddDoctor.php
@@ -11,8 +11,9 @@ include_once "../class/Doctor.php";
 if (isset($_POST["lastname"])) {
     $doctor = new Doctor(null, $_POST["lastname"], $_POST["firstname"], $_POST["street"], $_POST["suburb"], $_POST["city"], $_POST["phone"], $_POST["speciality"], $_POST["salary"]);
     $doctor->save();
-    $addSuccess = true;
-    history.back();
+    $addSuccess = true;?>
+    <script>history.back();</script>
+    <?php
 }else{
     ?>
     <a class="btn btn-danger" onclick="toastr.error('Hi! I am error message.');">Error message</a>
