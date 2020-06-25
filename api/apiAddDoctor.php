7f9ee9da15203f6f52417efe04dc886f44d51c34
Mojeeb <noutam01@myunitec.ac.nz>
2020-06-25 13:36:49 +1200

    a

--- api/apiAddDoctor.php
+++ api/apiAddDoctor.php
@@ -11,7 +11,14 @@ include_once "../class/Doctor.php";
 if (isset($_POST["lastname"])) {
     $doctor = new Doctor(null, $_POST["lastname"], $_POST["firstname"], $_POST["street"], $_POST["suburb"], $_POST["city"], $_POST["phone"], $_POST["speciality"], $_POST["salary"]);
     $doctor->save();?>
-    <script>history.back(); $('#addSuccess').toggleClass('display-none');</script>
+    <div id="addDoctor">Doctor Added Successfully!</div>
+    <script>
+        function addDoctor() {
+            var x = document.getElementById("addDoctor");
+            x.className = "show";
+            setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+        }
+    </script>
     <?php
 }else{
     $msg = "doctor not added";
