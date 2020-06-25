12f3e8dd20055b6040740700c46bc933b4856b16
Mojeeb <noutam01@myunitec.ac.nz>
2020-06-25 14:07:25 +1200

    a

--- api/apiAddDoctor.php
+++ api/apiAddDoctor.php
@@ -10,10 +10,8 @@ include_once "../class/Doctor.php";
 
 if (isset($_POST["lastname"])) {
     $doctor = new Doctor(null, $_POST["lastname"], $_POST["firstname"], $_POST["street"], $_POST["suburb"], $_POST["city"], $_POST["phone"], $_POST["speciality"], $_POST["salary"]);
-    $doctor->save();?>
-    <?php
+    $doctor->save();
 }else{
-    $msg = "doctor not added";
 }
 $msg = json_encode($msg);
 echo $msg;
