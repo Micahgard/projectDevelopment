b3626b5129cb0e8621f3d691d5461a177927aa37
BilmondNight <zhd.night@gmail.com>
2020-06-25 13:20:23 +1200

    close admission done

--- api/apiCloseBilled.php
+++ api/apiCloseBilled.php
@@ -6,10 +6,14 @@
  * Purpose: api for changing admission's status to close
  */
 
-include_once "../class/Billed.php";
-    $id = $_POST['id'];
-    $status = "billed";
-    $invoice = new Billed($id, $status);
-    $invoice->invoice();
+include_once "../class/Admission.php";
+
+if (isset($_GET['id'])) {
+    $admission = new Admission($_GET["id"], "", "", "", "","");
+    $admission->closeAndDelete();
+    $msg = "admission closed";
+}else{
+    $msg = "admission not closed";
+}
 $msg = json_encode($msg);
 echo $msg;
