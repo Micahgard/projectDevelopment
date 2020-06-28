df61bf5d6b1dfb9f3b84491fe981a065bd2b8147
Mojeeb <noutam01@myunitec.ac.nz>
2020-06-28 23:24:08 +1200

    a

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -8,9 +8,3 @@ function notificationGoBack() {
             //window.history.back();
         }, 6000);
 }
-
-function showNotification() {
-    let x = document.getElementById("notification");
-    x.className = "show";
-    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
-}
