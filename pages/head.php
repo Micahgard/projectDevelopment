50a35d76afe16f7fd5cbb4dbcb32b3f94da6989f
Mojeeb <noutam01@myunitec.ac.nz>
2020-06-30 00:01:02 +1200

    a

--- pages/head.php
+++ pages/head.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="../styles/css/global.css" rel="stylesheet">
+    <script src="../scripts/scripts.js"></script>
     <!-- Latest CSS -->
     <link rel="stylesheet" href="../styles/css/bootstrap.css">
     <!-- jQuery library -->
@@ -12,7 +13,6 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <!-- Latest compiled JavaScript -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
-    <script src="../scripts/scripts.js"></script>
     <link rel="icon" href="../image/icon.png">
 </head>
 <body>
