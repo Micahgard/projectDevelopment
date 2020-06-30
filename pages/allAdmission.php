538312c88938f13640b5422f1aac1af7fb119d17
Mojeeb <noutam01@myunitec.ac.nz>
2020-06-30 23:23:35 +1200

    a

--- pages/allAdmission.php
+++ pages/allAdmission.php
@@ -321,9 +321,9 @@
                             i = 0;
                             while (i < data.length) {
                                 if (data[i].AdmissionID == $("#getInvoice").val()) {
-                                    $("#invoice").append("<table class='table table-borderless'><tr><td>" + data[i].patient.id + "</td><td>"
-                                        + data[i].patient.firstname + " " + data[i].patient.lastname + "</td></tr><tr><td>"
-                                        + data[i].patient.address + "</td></tr>");
+                                    $("#invoice").append("<tr><td>Patient ID:</td><td>" + data[i].patient.id + "</td>" +
+                                        "<td>Patient Name:</td><td>" + data[i].patient.firstname + " " + data[i].patient.lastname + "</td></tr>");
+                                    $("#invoice").append("<tr><td>Patient Address:</td><td>" + data[i].patient.address + "</td></tr>");
                                     m = 0;
                                     subtotal = 0;
                                     while (m < data[i].medication.length) {
