00c82ddbf6159b6cf6ec00b5f299292eb45dfb01
Mojeeb <noutam01@myunitec.ac.nz>
2020-06-30 20:51:35 +1200

    a

--- pages/allAdmission.php
+++ pages/allAdmission.php
@@ -307,7 +307,7 @@
         <br><br><br>
     </div>
 
-    <div id="produceInvoice" class="container tab-pane fade"><br>
+    <div id="produceInvoice" class="container tab-pane fade" style="width: 100px"><br>
         <script>
             $(document).ready(function () {
                 $.ajax({
